0ed893ce7bb64f8cc7758f51fc35e05b38dc579c
Keegan Miller <kmmiller06@indianatech.net>
2025-12-03 19:17:14 -0500

    Renamed HW 7

---
+++ Classwork/Week14.py
@@ -0,0 +1,19 @@
+# List the functions you'd need for an ATM program
+
+# Each function should have one clear purpose
+# Example: check_pin(), get_balance(), withdraw_cash()
+# List at least 5 functions
+def check_pin(pin):
+    pass
+
+def get_balance(account_id):
+    pass
+
+def withdraw_cash(account_id, amount):
+    pass
+
+def deposit_cash(account_id, amount):
+    pass
+
+def print_receipt(transaction_details):
+    pass
